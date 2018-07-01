ec90a0deed0940db11bfa687c052bc130824cca2
Adan De Santiago Avalos <adan1995a@gmail.com>
2018-07-01 01:25:41 -0500

    registro de eventos modificable con resumen

--- server/server.js
+++ server/server.js
@@ -31,16 +31,105 @@ app.post('/evento', function (req, res) {
 
   referenciaPush.set({
     sorteo:req.body.sorteo,
+    modalidad:req.body.modalidad,
+    tipo:req.body.tipo,
     combinacion:req.body.combinacion,
     cantidadApostada: req.body.cantidadApostada,
+    ganado:req.body.ganado,
     cantidadGanada:req.body.cantidadGanada,
-    fecha:dia+'/'+mes+'/'+anio
+    fecha:dia+'/'+mes+'/'+anio,
+    key:referenciaPush.key
 
   });
   res.send("dato subido");
 });
 
+app.post('/evento-updateGan', function (req, res) {
+
+  var dia;
+  var mes;
+  var anio;
+  var cont=0;
+  var aux=0;
+  var date=req.body.fecha;
+
+  for(var i=0;i<date.length;i++){
+      if(date[i]=='/'){
+        if(cont==0){
+          dia=date.substring(aux,i);
+          aux=i+1;
+          cont++;
+        }
+        else if(cont==1){
+          mes=date.substring(aux,i);
+          aux=i+1;
+          cont++;
+        }
+      }
+     else if(i==date.length-1){
+        anio=date.substring(aux,date.length);
+      }
+  }
+  var ref =dataBase.ref('analitics/registros/'+anio+'/'+mes+'/'+dia+'/'+req.body.key);
+  var cantidad=req.body.ganado?calcularMonto(req.body.tipo,req.body.modalidad,req.body.cantidadApostada):0;
+
+  ref.update({
+    ganado:req.body.ganado,
+    cantidadGanada:cantidad,
+  });
+  res.send("dato subido");
+});
+
+function calcularMonto(tipo,modalidad,cantidad){
+  var monto=0;
+  if(tipo==1){
+    switch (modalidad) {
+      case 1:
+        monto=cantidad*50000;
+        break;
+      case 2:
+        monto=cantidad*5000;
+        break;
+      case 3:
+        monto=cantidad*500;
+        break;
+      case 4:
+        monto=cantidad*50;
+        break;
+      case 5:
+        monto=cantidad*50;
+        break;
+      default:
+        monto=cantidad*5;
+    }
+  }
+  else if(tipo==2){
+    switch (modalidad) {
+      case 1:
+        monto=cantidad*80000;
+        break;
+      case 2:
+        monto=cantidad*8000;
+        break;
+      case 3:
+        monto=cantidad*800;
+        break;
+      case 4:
+        monto=cantidad*80;
+        break;
+      case 5:
+        monto=cantidad*80;
+        break;
+      default:
+        monto=cantidad*8;
+    }
+  }
+  console.log(monto);
+  return monto;
+}
+
 app.post('/eventos-descarga', function (req, res) {
+  console.log(req.body.mes);
   var referencia=dataBase.ref('analitics/registros/'+req.body.anio+'/'+req.body.mes);
   var datos=[];
   var promise =new Promise(
@@ -51,8 +140,8 @@ app.post('/eventos-descarga', function (req, res) {
             if(snapChild.exists()){
               snapChild.forEach(snapBaby=>{
                 if(snapBaby.exists()){
-                  resolve(datos=datos.concat([{fecha:snapBaby.val().fecha,sorteo:snapBaby.val().sorteo,
-                    combinacion:snapBaby.val().combinacion,cantidadApostada:snapBaby.val().cantidadApostada,
+                  resolve(datos=datos.concat([{fecha:snapBaby.val().fecha,sorteo:snapBaby.val().sorteo,modalidad:snapBaby.val().modalidad,tipo:snapBaby.val().tipo,
+                    combinacion:snapBaby.val().combinacion,cantidadApostada:snapBaby.val().cantidadApostada,ganado:snapBaby.val().ganado,key:snapBaby.val().key,
                     cantidadGanada:snapBaby.val().cantidadGanada,total:snapBaby.val().cantidadGanada-snapBaby.val().cantidadApostada}]));
                 }
                 else{
@@ -67,6 +156,7 @@ app.post('/eventos-descarga', function (req, res) {
   )
   promise.then(
     function(){
+      console.log(datos.fecha+' '+datos.total);
       res.send(datos);
     }
   )
--- src/eventos.js
+++ src/eventos.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Form, Input } from 'semantic-ui-react';
-import { Header, Icon,Image, Statistic ,Table} from 'semantic-ui-react';
+import { Header, Icon,Image, Statistic ,Table,Dropdown} from 'semantic-ui-react';
 import './index.css';
 import Button from '@material-ui/core/Button';
 import AddIcon from '@material-ui/icons/Add';
@@ -10,6 +10,23 @@ import DialogTitle from '@material-ui/core/DialogTitle';
 import Dialog from '@material-ui/core/Dialog';
 import Save from '@material-ui/icons/Save';
 import axios from 'axios';
+import DialogActions from '@material-ui/core/DialogActions';
+import Switch from '@material-ui/core/Switch';
+
+const options = [
+  { key: 1, text: 'Directa 5', value: 1 },
+  { key: 2, text: 'Directa 4', value: 2 },
+  { key: 3, text: 'Directa 3', value: 3 },
+  { key: 4, text: 'Par inicial', value: 4},
+  { key: 5, text: 'Par final', value: 5 },
+  { key: 6, text: 'Inicial', value: 6},
+  { key: 7, text: 'Final', value: 7 },
+
+]
+const tipoJuego=[
+  { key: 1, text: 'Oficial', value: 1},
+  { key: 2, text: 'Alternativo', value: 2 },
+]
 
 class Eventos extends Component{
   constructor(props){
@@ -20,10 +37,13 @@ class Eventos extends Component{
       sorteo:'',
       combinacion:'',
       cantidad:'',
-      cantidadGanada:''
+      cantidadGanada:'0',
+      ganado:false,
+      datosListos:false
+
     };
   }
-  componentWillMount(){
+  componentDidMount(){
     this.tomarDatosTabla();
   }
 
@@ -36,9 +56,50 @@ class Eventos extends Component{
         console.log(res.data[0]);
         this.setState({
           datosItem:res.data
-        })
+        });
+        this.separarDatosEstadisticas(res.data);
+
       })
   }
+   intlRound=(numero, decimales , usarComa )=>{
+    var opciones = {
+        maximumFractionDigits: decimales,
+        useGrouping: false
+    };
+    usarComa = usarComa ? "es" : "en";
+    return new Intl.NumberFormat(usarComa, opciones).format(numero);
+}
+
+  separarDatosEstadisticas=(datos)=>{
+    var datos=datos;
+    var sorteos=0;
+    var inversion=0;
+    var ganados=0;
+    var cantidadGanada=0;
+    var Rentabilidad=0;
+    var datosEstadistica=[];
+    for(var i=0;i<datos.length;i++){
+        sorteos++;
+        inversion+=parseInt(datos[i].cantidadApostada);
+        cantidadGanada+=parseInt(datos[i].cantidadGanada);
+        if(datos[i].ganado){
+          ganados++;
+        }
+    }
+    Rentabilidad=this.intlRound(((cantidadGanada-inversion)/cantidadGanada)*100,3,false);
+    datosEstadistica={
+      sorteos:sorteos,
+      inversion:inversion,
+      ganados:ganados,
+      cantidadGanada:cantidadGanada,
+      rentabilidad:Rentabilidad
+    };
+    this.setState({
+      datosEstadistica:datosEstadistica,
+      datosListos:true
+    })
+  }
+
   handleClose=()=>{
     this.setState({
       open:false
@@ -67,7 +128,7 @@ class Eventos extends Component{
 
   handleRef2=(e)=>{
     var valor=e.target.value;
-    if(!isNaN(valor)){
+    if(!isNaN(valor)|valor==='X'){
     this.setState({
       combinacion:valor
     })
@@ -86,15 +147,39 @@ class Eventos extends Component{
       cantidad:valor
     })
   }
-  handleRef4=(e)=>{
-    var valor=e.target.value;
-    this.setState({
-      cantidadGanada:valor
-    })
-  }
+
   subirForm=()=>{
+    var value=this.state.value;
+    var comb=this.state.combinacion;
+    var combinacion;
+    switch (value) {
+      case 2:
+
+        combinacion='X'+comb;
+        break;
+      case 3:
+        combinacion='XX'+comb;
+        break;
+      case 4:
+          combinacion=comb+'XXX';
+          break;
+      case 5:
+        combinacion='XXX'+comb;
+          break;
+      case 6:
+          combinacion=comb+"XXXX";
+          break;
+      case 7:
+          combinacion='XXXX'+comb;
+          break;
+      default:
+        combinacion=comb;
+
+    }
+
     axios.post(`http://localhost:4000/evento`,
-      { sorteo:this.state.sorteo,combinacion:this.state.combinacion,cantidadApostada:this.state.cantidad,cantidadGanada:this.state.cantidadGanada })
+      { sorteo:this.state.sorteo,combinacion:combinacion,cantidadApostada:this.state.cantidad,
+        cantidadGanada:this.state.cantidadGanada,tipo:this.state.tipo,modalidad:this.state.value,ganado:this.state.ganado })
       .then(res => {
         alert(res.data);
         this.setState({
@@ -102,13 +187,29 @@ class Eventos extends Component{
           cantidad:'',
           combinacion:'',
           sorteo:'',
-          open:false
+          open:false,
+          datosItem:[]
         })
+        this.tomarDatosTabla();
       })
 
   }
+  handleChange = (e, { value }) =>{
+     this.setState({
+       value:value,
+       combinacion:''
+      })
+   }
+   handleChange2 = (e, { value }) =>{
+      this.setState({
+        tipo:value
+       })
+    }
 
   render(){
+    const {value}=this.state
+    const{tipo}= this.state
+    const estadisticaLista=this.state.datosListos;
     return(
       <div className="contenedorEventos">
       <Header as='h2' className="encabezado">
@@ -121,8 +222,11 @@ class Eventos extends Component{
           <Table.Row>
             <Table.HeaderCell> Fecha</Table.HeaderCell>
             <Table.HeaderCell>Sorteo</Table.HeaderCell>
+            <Table.HeaderCell>Tipo</Table.HeaderCell>
+            <Table.HeaderCell>Modalidad</Table.HeaderCell>
             <Table.HeaderCell>Combinacion</Table.HeaderCell>
             <Table.HeaderCell>$ Cantidad apostada</Table.HeaderCell>
+            <Table.HeaderCell>Ganador</Table.HeaderCell>
             <Table.HeaderCell>$ Cantidad ganada</Table.HeaderCell>
             <Table.HeaderCell>Total</Table.HeaderCell>
 
@@ -130,8 +234,8 @@ class Eventos extends Component{
         </Table.Header>
 
         <Table.Body>
-          {this.state.datosItem.map((it)=>{
-            return(<Item fila={it}/>)
+          {this.state.datosItem.map((it,key)=>{
+            return(<Item fila={it} i={key} actualizarLista={this.tomarDatosTabla}/>)
           })}
         </Table.Body>
       </Table>
@@ -145,8 +249,34 @@ class Eventos extends Component{
             <DialogContent>
               <div className='formulario-evento'>
                   <div className='elemento-form'>
+                       <label >Tipo</label>
+                       <div className='dropTipoSorteo'>
+                          <Dropdown
+                             onChange={this.handleChange2}
+                             options={tipoJuego}
+                             placeholder='elige una opcion'
+                             selection
+                             value={tipo}
+                           />
+                        </div>
+                  </div>
+
+                  <div className='elemento-form'>
                        <label >Sorteo</label>
                        <Input fluid placeholder='Sorteo' onChange={this.handleRef1} value={this.state.sorteo}/>
+                  </div>
+
+                  <div className='elemento-form'>
+                       <label >Modalidad</label>
+                       <div className='dropTipoSorteo'>
+                          <Dropdown
+                             onChange={this.handleChange}
+                             options={options}
+                             placeholder='elige una opcion'
+                             selection
+                             value={value}
+                           />
+                        </div>
                   </div>
 
                   <div className='elemento-form'>
@@ -158,10 +288,7 @@ class Eventos extends Component{
                        <Input fluid placeholder='$ 0.00' onChange={this.handleRef3} value={this.state.cantidad} />
                   </div>
 
-                   <div className='elemento-form'>
-                       <label>Cantidad ganada</label>
-                       <Input fluid placeholder='$ 0.00' onChange={this.handleRef4} value={this.state.cantidadGanada}/>
-                    </div>
+
                        <div className='btn-guardar'>
                          <Button variant="contained" color="secondary" onClick={this.subirForm}>
                           <Save />
@@ -174,66 +301,147 @@ class Eventos extends Component{
          <Button variant="fab" color="secondary" aria-label="add" onClick={this.abrir}>
           <AddIcon />
         </Button>
+        {estadisticaLista?
          <div className='estadisticas'>
-          <StatisticExampleValue />
-         </div>
+          <StatisticExampleValue datos={this.state.datosEstadistica}/>
+         </div>:
+         <div></div>
+       }
         </div>
     )
   }
 }
 
 
-const StatisticExampleValue = () => (
-  <Statistic.Group>
-    <Statistic>
-      <Statistic.Value>22</Statistic.Value>
-      <Statistic.Label>Saves</Statistic.Label>
-    </Statistic>
-
-    <Statistic>
-      <Statistic.Value text>
-        Three<br />
-        Thousand
-      </Statistic.Value>
-      <Statistic.Label>Signups</Statistic.Label>
-    </Statistic>
-
-    <Statistic>
-      <Statistic.Value>
-        <Icon name='plane' />
-        5
-      </Statistic.Value>
-      <Statistic.Label>Flights</Statistic.Label>
-    </Statistic>
-
-    <Statistic>
-      <Statistic.Value>
-        <Image src='https://react.semantic-ui.com/assets/images/avatar/small/joe.jpg' inline circular />
-        42
-      </Statistic.Value>
-      <Statistic.Label>Team Members</Statistic.Label>
-    </Statistic>
-  </Statistic.Group>
-)
-class Item extends Component{
+class StatisticExampleValue extends Component{
   constructor(props){
     super(props)
-    this.state={
-
-    }
 
   }
 
 
   render(){
     return(
+  <Statistic.Group>
+    <Statistic>
+      <Statistic.Value>{this.props.datos.sorteos}</Statistic.Value>
+      <Statistic.Label>Sorteos</Statistic.Label>
+    </Statistic>
+
+    <Statistic>
+      <Statistic.Value text>
+        ${this.props.datos.inversion}<br />
+        Pesos
+      </Statistic.Value>
+      <Statistic.Label>Cantidad invertida</Statistic.Label>
+    </Statistic>
+
+    <Statistic>
+      <Statistic.Value>
+        <Icon name='trophy' />
+        {this.props.datos.ganados}
+      </Statistic.Value>
+      <Statistic.Label>Sorteos ganados</Statistic.Label>
+    </Statistic>
+
+    <Statistic>
+      <Statistic.Value>
+        ${this.props.datos.cantidadGanada}
+      </Statistic.Value>
+      <Statistic.Label>Cantidad ganada</Statistic.Label>
+    </Statistic>
+    <Statistic>
+      <Statistic.Value>
+
+        {this.props.datos.rentabilidad}%
+
+      </Statistic.Value>
+      <Statistic.Label>Rentabilidad</Statistic.Label>
+    </Statistic>
+  </Statistic.Group>
+  )
+ }
+}
+class Item extends Component{
+  constructor(props){
+    super(props)
+    this.state={
+      checked: this.props.fila.ganado,
+      open:false
+
+    }
+
+  }
+  handleChange = name => event => {
+    if(this.state.checked){
+      this.setState({checked:false,open:true});
+    }
+    else{
+    this.setState({checked:true,open:true});
+  }
+  };
+  handleClose=()=>{
+    this.setState({open:false});
+  }
+  cambiarBD=()=>{
+    axios.post(`http://localhost:4000/evento-updateGan`,
+      { key:this.props.fila.key,ganado:this.state.checked,modalidad:this.props.fila.modalidad,tipo:this.props.fila.tipo,
+        fecha:this.props.fila.fecha,cantidadApostada:this.props.fila.cantidadApostada })
+      .then(res => {
+        alert(res.data);
+        this.setState({
+          open:false,
+        })
+        this.props.actualizarLista();
+      })
+  }
+
+  render(){
+    const tipo=this.props.fila.tipo;
+    const tipoTabla=tipo==1?'Oficial':'Alternativo'
+    const modalidad=this.props.fila.modalidad;
+    const modalidadTabla=modalidad==1?'Directa 5':
+                         modalidad==2?'Directa 4':
+                         modalidad==3?'Directa 3':
+                         modalidad==4?'Par inical':
+                         modalidad==5?'Par final':
+                         modalidad==6?'Inicial':
+                         modalidad==7?'Final':0
+    return(
       <Table.Row>
           <Table.Cell>{this.props.fila.fecha}</Table.Cell>
           <Table.Cell>{this.props.fila.sorteo}</Table.Cell>
+          <Table.Cell>{tipoTabla}</Table.Cell>
+          <Table.Cell>{modalidadTabla}</Table.Cell>
           <Table.Cell>{this.props.fila.combinacion}</Table.Cell>
-          <Table.Cell>{this.props.fila.cantidadGanada}</Table.Cell>
           <Table.Cell>{this.props.fila.cantidadApostada}</Table.Cell>
+          <Table.Cell>
+              <Switch
+              checked={this.state.checked}
+              onChange={this.handleChange('checked')}
+              value="checked"/>
+        </Table.Cell>
+        <Table.Cell>{this.props.fila.cantidadGanada}</Table.Cell>
           <Table.Cell>{this.props.fila.total}</Table.Cell>
+          <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          aria-labelledby="responsive-dialog-title">
+            <DialogTitle id="responsive-dialog-title">{"¿Seguro quieres modificarlo?"}</DialogTitle>
+              <DialogContent>
+                <DialogContentText>
+                  La base de datos marcará el evento como ganado
+                </DialogContentText>
+              </DialogContent>
+              <DialogActions>
+                <Button onClick={this.handleClose} color="primary">
+                  Cancelar
+                </Button>
+                <Button onClick={this.cambiarBD} color="primary" autoFocus>
+                  Aceptar
+                </Button>
+              </DialogActions>
+        </Dialog>
       </Table.Row>
 
 
